b5e04fd534efa4954a3863ed6eaedb2759b20593
HAWKEZHK <HAWKEZHK@github.com>
2019-04-07 00:29:24 +0800

    feat: 初始化3D场景

---
+++ src/constants/index.ts
@@ -0,0 +1,12 @@
+// 最大值
+export const MAX_SIZE = 1000;
+export const STEP = 50;
+
+// 相机
+export const CAMARE = {
+  fov: 45, // 视角
+  near: 50, // 相机距离近裁剪面距离
+  far: 4000, // 相机距离远裁剪面距离
+  pos: { x: 1000, y: 1000, z: 1000 }, // 相机位置
+  lookAt: { x: 0, y: 0, z: 0 }, // 视点
+};
--- src/typings/global.d.ts
+++ src/typings/global.d.ts
@@ -2,3 +2,7 @@ declare module '*.less' {
   const classNames: { [key: string]: string };
   export default classNames;
 }
+declare module 'three-orbitcontrols' {
+  const module: any;
+  export default module;
+}
